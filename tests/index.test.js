1907ca43375781a0d60f7e71f138086e55cb1675
Hannes Aaltonen <hannes@rareagency.fi>
2020-12-05 23:33:50 +0200

    added tests

---
+++ tests/index.test.js
@@ -0,0 +1,8 @@
+const fetch = require('node-fetch');
+
+describe('Test HTTP server', () => {
+  it('Test that response status is 200', async () => {
+    const response = await fetch(`http://localhost:8081`);
+    expect(response.status).toBe(200);
+  });
+});
