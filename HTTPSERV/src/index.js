d3901deb0807c9f4ffca62eec6baace36f923e60
Hannes A <aaltonen.hannes@gmail.com>
2020-12-08 21:41:26 +0200

    remove unnecessary exposed ports

--- HTTPSERV/src/index.js
+++ HTTPSERV/src/index.js
@@ -2,21 +2,20 @@
 This service is responsible for serving message logs.
 */
 
-import http from 'http';
-import fs from 'fs';
+import http from "http";
+import fs from "fs";
 const PORT = 8082;
-const FILENAME = '../../data/data.txt';
+const FILENAME = "../../data/data.txt";
 http
   .createServer(async (req, res) => {
     try {
-      fs.readFile(FILENAME, 'utf8', (err, data) => {
-        if (err) throw err;
+      fs.readFile(FILENAME, "utf8", (err, data) => {
         res.end(data);
       });
     } catch (e) {
       console.error(e);
       res.statusCode = 500;
-      res.end('Server error 500');
+      res.end("Server error 500");
     }
   })
   .listen(PORT, () => {
